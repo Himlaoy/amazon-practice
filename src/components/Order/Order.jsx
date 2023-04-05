c70651f7445b14ed71e29486e5a518632484299e
himaloyash <himaloyashis98@gmail.com>
2023-04-05 18:12:39 +0600

    ok

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,27 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Cart from '../Cart/Cart';
-import { useLoaderData } from 'react-router-dom';
-import ReviewItem from '../ReviewItem/ReviewItem';
 import './Order.css'
-import { removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
-    const saveCard = useLoaderData()
-    const [card, setCard] = useState(saveCard)
-    const handleDelete =(id)=>{
-       const remaining = card.filter(pd=>pd.id !==id)
-       setCard(remaining)
-       removeFromDb(id)
-    }
     return (
-        <div className='shop-container'>
-            <div className='PPcart'>
-                {
-                    card.map(product=><ReviewItem product={product} key={product.id} handleDelete={handleDelete}></ReviewItem>)
-                }
+        <div className='card-Detail'>
+            <div>
+                <h2>This is my product</h2>
             </div>
-            <div className='cart-container'>
-                <Cart cart={card} ></Cart>
+            <div>
+                <Cart cart={[]}></Cart>
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Home from './components/Layout/Home';
+
 import Shop from './components/Shop/Shop';
 import Product from './components/Product/Product';
+import Home from './components/Home/Home';
 import Order from './components/Order/Order';
-import Longin from './components/Login/Longin';
-import cardProduct from './CardProduct/CardProduct';
 
 const router = createBrowserRouter([
   {
@@ -27,14 +26,10 @@ const router = createBrowserRouter([
         element: <Product></Product>
       },
       {
-        path:'order',
-        element:<Order></Order>,
-        loader: cardProduct,
-      },
-      {
-        path:'login',
-        element: <Longin></Longin>
+        path: 'order',
+        element: <Order></Order>,
       }
+      
     ]
   },
 
